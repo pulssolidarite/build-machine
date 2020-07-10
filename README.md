47116467650ffecc5686fb23c8897897cea7f29b
pulssolidarite <contact@puls-impact.fr>
2020-07-10 19:09:14 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+**Install for the AppImage build machine**
+
+run the run-me.sh script for fast install of :
+- dotnet SDK
+- mono
+- nodejs
+
---
+++ run-me.sh
@@ -0,0 +1,21 @@
+# stay updated
+sudo apt -y update
+sudo apt -y upgrade
+
+# install dotnet in ~/.dotnet with sdk
+bash dotnet-install.sh
+echo "export DOTNET_ROOT=$HOME/.dotnet" >> $HOME/.bashrc
+echo "export PATH=$PATH:$HOME/.dotnet" >> $HOME/.bashrc
+
+# install mono
+sudo apt install apt-transport-https dirmngr gnupg ca-certificates
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+echo "deb https://download.mono-project.com/repo/debian stable-raspbianbuster main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list
+sudo apt update
+sudo apt install mono-complete
+
+#install nodejs
+curl -sL https://deb.nodesource.com/setup_12.x -o nodesource_setup.sh
+sudo bash nodesource_setup.sh
+sudo apt install nodejs build-essential
+
